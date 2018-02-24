ddec5009a3259ac61cf98b60c722f116f4be16c1
sofiane <sofiane_belhadj_kacem@outlook.fr>
2018-02-24 21:00:15 +0100

    Deuxieme verssion terminé.

--- Client/weather.js
+++ Client/weather.js
@@ -15,90 +15,75 @@ $("document").ready(function() {
       url: "../Serveur/index.php",
       data: data,
       success: function(data) {
-        icon1 = "http://openweathermap.org/img/w/" + data["Icon1"] + ".png";
-        icon2 = "http://openweathermap.org/img/w/" + data["Icon2"] + ".png";
-        icon3 = "http://openweathermap.org/img/w/" + data["Icon3"] + ".png";
+        $("#weather").empty();
 
-        $("#city").empty();
-        $("#city").append(data["City"]);
-        $("#longitude").empty();
-        $("#longitude").append(data["longitude"] + " C");
-        $("#latitude").empty();
-        $("#latitude").append(data["latitude"] + " C");
-
-        $("#city2").empty();
-        $("#city2").append(data["City"]);
-        $("#longitude2").empty();
-        $("#longitude2").append(data["longitude"] + " C");
-        $("#latitude2").empty();
-        $("#latitude2").append(data["latitude"] + " C");
-
-        $("#city3").empty();
-        $("#city3").append(data["City"]);
-        $("#longitude3").empty();
-        $("#longitude3").append(data["longitude"] + " C");
-        $("#latitude3").empty();
-        $("#latitude3").append(data["latitude"] + " C");
-
-        //Jour1
-
-        $("#graphic1").empty();
-        $("#graphic1").append('<img src="' + icon1 + '"/>');
-        $("#description1").empty();
-        $("#description1").append(data["description1"]);
-        $("#windSpeed1").empty();
-        $("#windSpeed1").append(data["speedwind1"]);
-        $("#pressure1").empty();
-        $("#pressure1").append(data["pressure1"]);
-        $("#humidity1").empty();
-        $("#humidity1").append(data["humidity1"] + "%");
-        $("#celsius1").empty();
-        $("#celsius1").append(data["temp1"] + "C");
-        $("#min1").empty();
-        $("#min1").append(data["Max1"] + " C");
-        $("#max1").empty();
-        $("#max1").append(data["Min1"] + " C");
-        $("#date1").append(data["date1"]);
-
-        //Jour2
-
-        $("#graphic2").empty();
-        $("#graphic2").append('<img src="' + icon2 + '"/>');
-        $("#description2").empty();
-        $("#description2").append(data["description2"]);
-        $("#windSpeed2").empty();
-        $("#windSpeed2").append(data["speedwind2"]);
-        $("#pressure2").empty();
-        $("#pressure2").append(data["pressure2"]);
-        $("#humidity2").empty();
-        $("#humidity2").append(data["humidity2"] + "%");
-        $("#celsius2").empty();
-        $("#celsius2").append(data["temp2"] + "C");
-        $("#min2").empty();
-        $("#min2").append(data["Max2"] + " C");
-        $("#max2").empty();
-        $("#max2").append(data["Min2"] + " C");
-        $("#date2").append(data["date2"]);
-
-        //Jour3
-
-        $("#graphic3").empty();
-        $("#graphic3").append('<img src="' + icon3 + '"/>');
-        $("#description3").empty();
-        $("#description3").append(data["description3"]);
-        $("#windSpeed3").empty();
-        $("#windSpeed3").append(data["speedwind3"]);
-        $("#pressure3").empty();
-        $("#pressure3").append(data["pressure3"]);
-        $("#humidity3").empty();
-        $("#humidity3").append(data["humidity3"] + "%");
-        $("#celsius3").empty();
-        $("#celsius3").append(data["temp3"] + "C");
-        $("#min3").empty();
-        $("#min3").append(data["Max3"] + " C");
-        $("#max3").empty();
-        $("#max3").append(data["Min3"] + " C");
-        $("#date3").append(data["date3"]);
+        for (var i = 1; i < 20; i++) {
+          $("#weather").append(`
+          <div class="container">
+          <div class="title">
+            <h1 id="city">${data["City"]}</h1>
+            <div id="graphic" class="weather-icon"><img src="http://openweathermap.org/img/w/${
+              data[`Icon${i}`]
+            }.png"/></div>
+            <div id="description" class='important-info text'>${
+              data[`description${i}`]
+            }</div>
+            <div id="celsius" class="temp-info">${data[`temp${i}`]}</div>
+          </div>
+        </div>
+        <div class="description-container">
+          <div class="block-1">
+            <div class="text">
+              <span>Max</span>
+              <span id="max" class="important-info">${
+                data[`Max${i}`]
+              } °C </span>
+            </div>
+            <div class="text">
+              <span>Min</span>
+              <span id="min" class='important-info'>${
+                data[`Min${i}`]
+              } °C </span>
+            </div>
+            <div class="text">
+              <strong>
+                <span>Date</span>
+              </strong>
+              <strong>
+                <span id="date1" class='important-info'>${
+                  data[`date${i}`]
+                } </span>
+              </strong>
+            </div>
+    
+          </div>
+          <div class="block-2">
+            <span>Lon :</span>
+            <div id="longitude" class=" text important-info">${
+              data["longitude"]
+            }°C </div>
+            <span>Lat :</span>
+            <div id="latitude" class="text important-info"> ${
+              data["latitude"]
+            }°C</div>
+          </div>
+          <div class="block-3">
+            <span>Humidity:</span>
+            <div id="humidity" class="text important-info">${
+              data[`humidity${i}`]
+            } % </div>
+            <span>Pressure:</span>
+            <div id="pressure" class="text important-info">${
+              data[`pressure${i}`]
+            }°</div>
+            <span>Winds:</span>
+            <div id="windSpeed1" class="text important-info">${
+              data[`speedwind${i}`]
+            } Km/h</div>
+          </div>
+        </div>  
+          `);
+        }
 
         $(".imgmeteo").empty();
         $(".weather").css("visibility", "visible");
--- Serveur/index.php
+++ Serveur/index.php
@@ -17,14 +17,11 @@ function is_ajax() {
 function call_function(){
 
 // Get Meteo
-
-
-//$url = "http://api.openweathermap.org/data/2.5/weather?q=".$_POST["ville"]."&lang=en&units=metric&APPID=a570c3a84b7503ab4b1927647db754c9";
 $url = "http://api.openweathermap.org/data/2.5/forecast?q=".$_POST["ville"]."&units=metric&APPID=a570c3a84b7503ab4b1927647db754c9";
-//$url = "http://https://samples.openweathermap.org/data/2.5/forecast/daily?id=524901&APPID=a570c3a84b7503ab4b1927647db754c9";
 $contents = file_get_contents($url);
 $clima=json_decode($contents);
-  //print_r($clima);
+
+// Pour voir le Json print_r($clima);
   
   $city=$clima->city->name;
   $longitude = $clima->city->coord->lat;
@@ -32,82 +29,14 @@ $clima=json_decode($contents);
   $country = $clima->city->country;
   $population = $clima->city->population;
   
-  // //Jour1
-  // $temp1 = $clima->list[0]->main->temp;
-  // $temp_max1=$clima->list[0]->main->temp_max;
-  // $temp_min1=$clima->list[0]->main->temp_min;
-  // $pressure1 = $clima->list[0]->main->pressure;  
-  // $sea_level1 = $clima->list[0]->main->sea_level; 
-  // $humidity1 = $clima->list[0]->main->humidity;   
-  // $main1 = $clima->list[0]->weather[0]->main;
-  // $description1 = $clima->list[0]->weather[0]->description;
-  // $icon1=$clima->list[0]->weather[0]->icon;
-  // $speedwind1 = $clima->list[0]->wind->speed;
-  // $degwind1 = $clima->list[0]->wind->deg;
-
-  // //Jour2
-  // $temp2 = $clima->list[1]->main->temp;
-  // $temp_max2=$clima->list[1]->main->temp_max;
-  // $temp_min2=$clima->list[1]->main->temp_min;
-  // $pressure2 = $clima->list[1]->main->pressure;  
-  // $sea_level2 = $clima->list[1]->main->sea_level; 
-  // $humidity2 = $clima->list[1]->main->humidity;   
-  // $main2 = $clima->list[1]->weather[0]->main;
-  // $description2 = $clima->list[1]->weather[0]->description;
-  // $icon2 = $clima->list[1]->weather[0]->icon;
-  // $speedwind2 = $clima->list[1]->wind->speed;
-  // $degwind2 = $clima->list[1]->wind->deg;
-
-  // //Jour3
-  // $temp3 = $clima->list[2]->main->temp;
-  // $temp_max3 =$clima->list[2]->main->temp_max;
-  // $temp_min3 =
-  // $pressure3 = 
-  // $sea_level3 = $clima->list[2]->main->sea_level; 
-  // $humidity3 =  
-  // $main3 = 
-  // $description3 = 
-  // $icon3 =
-  // $speedwind3 = 
-  // $degwind3 = 
 
 $return = $_POST;
 
 $return["City"] = $city;
 $return["longitude"] = $longitude;
 $return["latitude"] = $latitude;
-$return["country"] = $country; //a utiliser
-$return["population"] = $population; //a utiliser
-
-// //Jour1 
-
-// $return["temp1"] = $temp1;
-// $return["Max1"] = $temp_max1;
-// $return["Min1"] = $temp_min1;
-// $return["pressure1"] = $pressure1;
-// $return["sea_level1"] = $sea_level1;
-// $return["humidity1"] = $humidity1;
-// $return["main1"] = $main1;
-// $return["description1"] = $description1;
-// $return["Icon1"] = $icon1;
-// $return["speedwind1"] = $speedwind1;
-// $return["degwind1"] = $degwind1;
-
-// //Jour2
-
-// $return["temp2"] = $temp2;
-// $return["Max2"] = $temp_max2;
-// $return["Min2"] = $temp_min2;
-// $return["pressure2"] = $pressure2;
-// $return["sea_level2"] = $sea_level2;
-// $return["humidity2"] = $humidity2;
-// $return["main2"] = $main2;
-// $return["description2"] = $description2;
-// $return["Icon2"] = $icon2;
-// $return["speedwind2"] = $speedwind2;
-// $return["degwind2"] = $degwind2;
-
-// //Jour3
+$return["country"] = $country; 
+$return["population"] = $population; 
 
 
 
